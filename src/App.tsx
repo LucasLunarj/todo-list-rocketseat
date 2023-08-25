2e78f4d63d2d49e58064aeffee9608700baf2cc6
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-08-25 02:07:28 -0300

    new functions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+//Hook
+import { useState } from "react";
 //components
 import { Header } from "./components/Header";
 import { TaskBar } from "./components/TaskBar";
 import { Tasks } from "./components/Tasks";
 //CSS
 import "./App.css";
+
 export function App() {
+  const [list, setList] = useState([
+    {
+      task: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+      checked: false,
+    },
+  ]);
+
+  function listGenerator(dataList) {
+    return setList([...list, { task: dataList, checked: false }]);
+  }
+
   return (
     <div>
       <Header />
       <div className="container">
         <div className="taskbar">
-          <TaskBar />
+          <TaskBar taskGenerator={listGenerator} />
         </div>
         <div className="tasks">
           <Tasks />
--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,16 +1,23 @@
 import styles from "./TaskBar.module.css";
-
+import { useState } from "react";
 //image
 
 import plus from "../assets/plus.svg";
 
-export const TaskBar = () => {
+export const TaskBar = ({ taskGenerator }) => {
+  const [data, setData] = useState("");
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.buttonBox}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button className={styles.buttonBox}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+          />
+          <button onClick={() => taskGenerator(data)} className={styles.buttonBox}>
             Criar
             <div>
               <img src={plus} alt="" />
